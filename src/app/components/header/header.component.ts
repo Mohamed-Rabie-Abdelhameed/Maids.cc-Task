1d6afa7224ef835ca142e9392477b22bebe5eeca
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-01-26 15:13:14 +0200

    set the base for header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  searchTerm: string = '';
+  searchResults: string[] = [];
+  searchSubject: Subject<string> = new Subject<string>();
 
+  constructor(private apiService: ApiService) {
+    this.searchSubject
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(() => {
+        this.apiService.getUsers(1).subscribe((results) => {
+          this.searchResults = results;
+          console.log(this.searchResults);
+        });
+      });
+  }
+
+  searchUser() {
+    if (this.searchTerm.length === 0) {
+      this.searchResults = [];
+      return;
+    }
+    this.searchSubject.next(this.searchTerm);
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  usersUrl: string = 'https://reqres.in/api/users?page=';
+  userDetailsUrl: string = 'https://reqres.in/api/users/';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(page: number): Observable<any> {
+    return this.http.get(this.usersUrl + page);
+  }
+
+  getUserDetails(userId: number): Observable<any> {
+    return this.http.get(this.userDetailsUrl + userId);
+  }
+}
