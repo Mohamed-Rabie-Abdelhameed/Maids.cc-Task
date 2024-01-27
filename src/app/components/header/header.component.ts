5742fa6ec7973b40619eaacc3f085f575fe14c30
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-01-27 15:43:47 +0200

    implement search functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   searchTerm: string = '';
-  searchResults: string[] = [];
+  searchResults: any[] = [];
+  totalPages: number = 2;
+  allUsers: any[] = [];
   searchSubject: Subject<string> = new Subject<string>();
 
   constructor(private apiService: ApiService) {
     this.searchSubject
       .pipe(debounceTime(500), distinctUntilChanged())
       .subscribe(() => {
-        this.apiService.getUsers(2).subscribe((results) => {
-          this.searchResults = results;
-          console.log(this.searchResults);
-        });
+        this.filterUsers();
+        console.log(this.searchResults);
       });
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      for (let i = 1; i <= this.totalPages; i++) {
+        this.getUsers(i);
+      }
+    }, 1000);
+  }
+
+  getUsers(page: number) {
+    this.apiService.getUsers(page).subscribe((results) => {
+      this.allUsers = this.allUsers.concat(results.data);
+      this.totalPages = results.total_pages;
+      console.log(this.allUsers);
+    });
+  }
+
+  filterUsers() {
+    this.searchResults = this.allUsers.filter((user: any) => {
+      return user.id.toString().includes(this.searchTerm);
+    });
+  }
   searchUser() {
     if (this.searchTerm.length === 0) {
       this.searchResults = [];
